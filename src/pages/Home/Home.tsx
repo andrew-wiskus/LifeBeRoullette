9490d3deb8ec8671136bb9d657ad4c17be2b410e
drewwiskus <drew.wiskus@space150.com>
2021-12-07 18:57:08 -0600

    add latency check and add/remove functionality to tick

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,23 @@
 import { inject, observer } from 'mobx-react';
 import React, { CSSProperties } from 'react';
+import { TimeStore } from '../../stores/TimeStore';
 import { UserStore } from '../../stores/UserStore'
-
 export interface StyleObject { [key: string] : CSSProperties }
  
-@inject('userStore')
+@inject('userStore', 'timeStore')
 @observer
-export class Home extends React.Component<{userStore?: UserStore }> {
+export class Home extends React.Component<{userStore?: UserStore, timeStore?: TimeStore }> {
+
+  private onClick = () => {
+    let inc = Math.floor(Math.random() * 1000)
+    this.props.timeStore!.addTickFunction('test_' + inc, () => console.log('works!! ' + 'test_' + inc))
+  }
 
   public render(): JSX.Element {
     return (
       <div style={styles.container}>
         <h1>home</h1>
+        <button onClick={() => this.onClick()}>TEST ADD</button>
       </div>
     );
   }
